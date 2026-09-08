60b3011a31d644d77a3ecbcb6d9e1c09edb5015e
shreedivya0884-stack <shreedivya0884@gmail.com>
2026-09-08 11:30:46 +0530

    Add files via upload

---
+++ eightmultiplication.java
@@ -0,0 +1,10 @@
+public class eightmultiplication {
+     public static void main(String[] args) {
+
+        int n = 8;
+
+        for(int i = 1; i <= 10; i++) {
+            System.out.println(n + " x " + i + " = " + (n * i));
+        }
+    }
+}
